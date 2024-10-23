84cf9d29e9864610a9ced87943a3abd19ed35c7b
spindart <spindart@proton.me>
2024-10-22 21:52:43 -0300

    feat: 345. Reverse Vowels of a string

---
+++ src/ReverseVowels.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\LeetCode;
+
+class ReverseVowels
+{
+
+    /**
+     * @param string $s
+     * @return string
+     */
+    function reverseVowels($s)
+    {
+        $vowels = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'];
+        $vowelPositions = [];
+        $extractedVowels = [];
+
+        for ($i = 0; $i < strlen($s); $i++) {
+            if (in_array($s[$i], $vowels)) {
+                $vowelPositions[] = $i;
+                $extractedVowels[] = $s[$i];
+            }
+        }
+
+        $extractedVowels = array_reverse($extractedVowels);
+
+        foreach ($vowelPositions as $index => $position) {
+            $s[$position] = $extractedVowels[$index];
+        }
+
+        return $s;
+    }
+}
